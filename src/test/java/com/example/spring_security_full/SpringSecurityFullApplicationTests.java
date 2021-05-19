1db5420b383f68bafd3948e777ac2201bb5543ea
npanjehpour <naser.panjehpour@rdsysco.com>
2021-05-19 15:14:33 +0430

    Implement base of spring security

---
+++ src/test/java/com/example/spring_security_full/SpringSecurityFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_full;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
